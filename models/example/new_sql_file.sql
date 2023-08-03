18e0d964770fbd1c044551693ddac9b678e318a5
Amar Ghodke <129994095+Amar-ghodke@users.noreply.github.com>
2023-08-03 21:10:47 +0530

    added source jinja for dag and added new table

--- models/example/new_sql_file.sql
+++ models/example/new_sql_file.sql
@@ -1,30 +1,30 @@
 with Active as (
 select 
-EXTERNAL_ID_VOD_C
-,ID
-,RECORDTYPEID
-,FIRSTNAME
-,MIDDLE_VOD_C
-,LASTNAME
-,NAME
-,NPI_VOD_C
-,CREDENTIALS_VOD_C
-,PERSONEMAIL
-,GENDER_VOD_C
-,ACCOUNT_IDENTIFIER_VOD_C
-,FAX
-,ME_C
-,PHONE
-,PRIMARY_PARENT_VOD_C
-,SALUTATION
+x.EXTERNAL_ID_VOD_C
+,x.ID
+,x.RECORDTYPEID
+,x.FIRSTNAME
+,x.MIDDLE_VOD_C
+,x.LASTNAME
+,x.NAME
+,x.NPI_VOD_C
+,x.CREDENTIALS_VOD_C
+,x.PERSONEMAIL
+,x.GENDER_VOD_C
+,x.ACCOUNT_IDENTIFIER_VOD_C
+,x.FAX
+,x.ME_C
+,x.PHONE
+,x.PRIMARY_PARENT_VOD_C
+,x.SALUTATION
 --,SPECIALTY_2_VOD_C
-,SUFFIX_VOD_C
-,SYSTEMMODSTAMP
-,TARGET_C
-,WEBSITE
-from {{ source('GOOGLE_SHEETS', 'GOOGLE_SHEETS') }} 
+,x.SUFFIX_VOD_C
+,x.SYSTEMMODSTAMP
+,x.TARGET_C
+,x.WEBSITE
+from {{ source('GOOGLE_SHEETS', 'GOOGLE_SHEETS') }} X
 where id is not null and NPI_VOD_C is not null and len(NPI_VOD_C)=10 and CNX_ACCOUNT_STATUS_C='Active')
-select * from Active join {{ source('GOOGLE_SHEETS', 'Target') }}
+select * from Active join {{ source('GOOGLE_SHEETS', 'Target') }} Y
 
 /*
     Getting the records from google sheet where id,npi_vod_c is not null and 
