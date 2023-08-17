2eb759f1599160ccb074b273d65d4d3acbf55562
Amar Ghodke <amarghodke601@gmail.com>
2023-08-17 16:27:39 +0000

    formated

--- models/Stage/sql_join.sql
+++ models/Stage/sql_join.sql
@@ -1,54 +1,58 @@
-with Active as (
-select 
-x.EXTERNAL_ID_VOD_C
-,x.ID
-,x.RECORDTYPEID
-,x.FIRSTNAME
-,x.MIDDLE_VOD_C
-,x.LASTNAME
-,x.NAME
-,x.NPI_VOD_C
-,x.CREDENTIALS_VOD_C
-,x.PERSONEMAIL
-,x.GENDER_VOD_C
-,x.ACCOUNT_IDENTIFIER_VOD_C
-,x.FAX
-,x.ME_C
-,x.PHONE
-,x.PRIMARY_PARENT_VOD_C
-,x.SALUTATION
---,SPECIALTY_2_VOD_C
-,x.SUFFIX_VOD_C
-,x.SYSTEMMODSTAMP
-,x.TARGET_C
-,x.WEBSITE
-from {{ source('GOOGLE_SHEETS', 'GOOGLE_SHEETS') }} X
-where id is not null and NPI_VOD_C is not null and len(NPI_VOD_C)=10)
-select 
-EXTERNAL_ID_VOD_C
-,A.ID
-,A.RECORDTYPEID
-,A.FIRSTNAME
-,A.MIDDLE_VOD_C
-,A.LASTNAME
-,A.NAME
-,A.NPI_VOD_C
-,A.CREDENTIALS_VOD_C
-,A.PERSONEMAIL
-,A.GENDER_VOD_C
-,A.ACCOUNT_IDENTIFIER_VOD_C
-,A.FAX
-,A.ME_C
-,A.PHONE
-,A.PRIMARY_PARENT_VOD_C
-,A.SALUTATION
-,A.SUFFIX_VOD_C
-,A.SYSTEMMODSTAMP
-,A.TARGET_C
-,A.WEBSITE 
-from Active A left join {{ source('GOOGLE_SHEETS', 'Target') }} Y on A.NPI_VOD_C = Y.NPI
+with
+    active as (
+        select
+            x.external_id_vod_c,
+            x.id,
+            x.recordtypeid,
+            x.firstname,
+            x.middle_vod_c,
+            x.lastname,
+            x.name,
+            x.npi_vod_c,
+            x.credentials_vod_c,
+            x.personemail,
+            x.gender_vod_c,
+            x.account_identifier_vod_c,
+            x.fax,
+            x.me_c,
+            x.phone,
+            x.primary_parent_vod_c,
+            x.salutation,
+            -- ,SPECIALTY_2_VOD_C
+            x.suffix_vod_c,
+            x.systemmodstamp,
+            x.target_c,
+            x.website
+        from {{ source("GOOGLE_SHEETS", "GOOGLE_SHEETS") }} x
+        where id is not null and npi_vod_c is not null and len(npi_vod_c) = 10
+    )
+select
+    external_id_vod_c,
+    a.id,
+    a.recordtypeid,
+    a.firstname,
+    a.middle_vod_c,
+    a.lastname,
+    a.name,
+    a.npi_vod_c,
+    a.credentials_vod_c,
+    a.personemail,
+    a.gender_vod_c,
+    a.account_identifier_vod_c,
+    a.fax,
+    a.me_c,
+    a.phone,
+    a.primary_parent_vod_c,
+    a.salutation,
+    a.suffix_vod_c,
+    a.systemmodstamp,
+    a.target_c,
+    a.website
+from active a
+left join
+    {{ source("GOOGLE_SHEETS", "Target") }} y on a.npi_vod_c = y.npi
 
-/*
+    /*
     Getting the records from google sheet where id,npi_vod_c is not null and 
     lenght of npi is 10 alsoit will give the records which are active.
 
@@ -62,3 +66,4 @@ git push -u origin main
 git remote add origin git@github.com:Amar-ghodke/dbt_env.git
 
 */
+    
