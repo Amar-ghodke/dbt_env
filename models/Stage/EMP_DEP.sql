803453f9decaad13ce5be08397721d218620e589
Amar Ghodke <amarghodke601@gmail.com>
2023-08-18 05:27:43 +0000

    edit emp job

--- models/Stage/EMP_DEP.sql
+++ models/Stage/EMP_DEP.sql
@@ -1,9 +1,10 @@
 with emp as(
-    select * from {{source('ADHOC','EMPLOYEE')}}
+    select EMPLOYEEID, EMPLOYEENAME, DEPARTMENTID, MANAGERID
+    from {{source('ADHOC','EMPLOYEE')}}
 
 ),
 dep as(
 
-    selcet * from {{source('ADHOC','DEPARTMENT')}}
+    selcet DEPARTMENTID, DEPARTMENTNAME from {{source('ADHOC','DEPARTMENT')}}
 )
 select * from emp e join dep d on e.DEPARTMENTID = d.DEPARTMENTID
